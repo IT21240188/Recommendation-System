8c7dee844b5c86fc32803a70c35b11c9c25315bb
Janul <it21240188@my.sliit.lk>
2024-09-21 11:11:37 +0530

    homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const HomePage = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/users');
+      setUsers(response.data.contacts);
+    } catch (error) {
+      console.error('Error fetching contacts:', error);
+    }
+  };
+  return <div></div>;
+};
+
+export default HomePage;
